5926ef37dae8a2bb67bfe827aced70dba20e1e2f
Laurence Liu <liuxy6@gmail.com>
2016-07-18 14:17:08 +0800

    Rewrite auth.go

--- auth.go
+++ auth.go
@@ -7,49 +7,33 @@ import (
 	"errors"
 	"io"
 	"time"
+
+	"gopkg.in/redis.v4"
+
+	"github.com/laurence6/gtd.go/model"
 )
 
-var ErrPasswordNotFound = errors.New("Cannot find password 'password' in conf")
-
-func encPassword(password string) string {
+func EncPassword(password string) string {
 	h := sha256.Sum256([]byte(password))
 	return base64.StdEncoding.EncodeToString(h[:])
 }
 
-// CheckPassword returns true if password is valid.
-func CheckPassword(password string) (bool, error) {
-	if password == "" {
-		return false, nil
+// CheckPassword returns UserID if UserID and password are valid.
+func CheckPassword(userID, password string) (string, error) {
+	if userID == "" || password == "" {
+		return "", errors.New("Empty UserID or Password")
 	}
-	storedPassword, ok := conf["password"].(string)
-	if !ok {
-		return false, ErrPasswordNotFound
-	}
-	if encPassword(password) == storedPassword {
-		return true, nil
-	}
-	return false, nil
-}
 
-// CheckToken returns true if token is valid.
-func CheckToken(token string) (bool, error) {
-	err := redisClient.Get(GetNamespace("tok", token)).Err()
-	if err == nil {
-		return true, nil
-	}
-	if err == ErrRedisNil {
-		return false, nil
-	}
-	return false, err
-}
-
-// SetToken sets token. Use "token:xxx" as key. An expiration in second is taken.
-func SetToken(token string, expires int) error {
-	err := redisClient.Set(GetNamespace("tok", token), "0", time.Duration(expires)*1e9).Err()
+	user, err := model.GetUser(userID)
 	if err != nil {
-		return err
+		return "", err
 	}
-	return nil
+
+	if user.Password == password {
+		return userID, nil
+	}
+
+	return "", nil
 }
 
 // NewToken returns a new token.
@@ -57,4 +41,24 @@ func NewToken() string {
 	b := make([]byte, 32)
 	io.ReadFull(rand.Reader, b)
 	return base64.URLEncoding.EncodeToString(b)
+}
+
+// SetToken stores UserID and token in redis using "token:xxx" as key. An expiration in second is taken.
+func SetToken(userID, token string, expires int) error {
+	err := redisClient.Set(getNamespace("tok", token), userID, time.Duration(expires)*1e9).Err()
+	return err
+}
+
+// CheckToken returns UserID if token is valid.
+func CheckToken(token string) (string, error) {
+	userID, err := redisClient.Get(getNamespace("tok", token)).Result()
+	if err == nil {
+		return userID, nil
+	}
+
+	if err == redis.Nil {
+		return "", nil
+	}
+
+	return "", err
 }
