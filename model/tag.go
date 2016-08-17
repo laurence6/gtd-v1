efe6ac69380eb48e5778e48c55076ff2bf307535
Laurence Liu <liuxy6@gmail.com>
2016-08-17 19:12:18 +0800

    model: Add column flags to:
      * Avoid unnecessary database read/write
      * Control which fields can be read/updated

--- model/tag.go
+++ model/tag.go
@@ -1,45 +1,9 @@
 package model
 
-type Tag struct {
-	TableName struct{} `sql:"tag"`
+func GetTagsByUserID(userID string) ([]string, error) {
+	tags := []string{}
 
-	UserID string `sql:",pk"`
-	TaskID int64  `sql:",pk"`
-	Name   string `sql:",pk"`
-}
-
-type Tags []Tag
-
-func (tags Tags) String() string {
-	if len(tags) == 0 {
-		return ""
-	}
-	if len(tags) == 1 {
-		return tags[0].Name
-	}
-
-	l := len(tags) - 1
-	for n := 0; n < len(tags); n++ {
-		l += len(tags[n].Name)
-	}
-
-	b := make([]byte, l)
-	bp := copy(b, tags[0].Name)
-	for _, tag := range tags[1:] {
-		bp += copy(b[bp:], ",")
-		bp += copy(b[bp:], tag.Name)
-	}
-	return string(b)
-}
-
-func GetTagsByUserID(userID string) ([]Tag, error) {
-	tags := []Tag{}
-
-	err := DBConn.Model(&tags).
-		Column("name").
-		Where("tag.user_id = ?", userID).
-		Group("name").
-		Select()
+	_, err := DBConn.Query(&tags, "SELECT tag.name FROM tag WHERE tag.user_id = ? GROUP BY tag.name", userID)
 	if err != nil {
 		return nil, err
 	}
@@ -47,12 +11,10 @@ func GetTagsByUserID(userID string) ([]Tag, error) {
 	return tags, nil
 }
 
-func GetTagsByTaskID(taskID int64) ([]Tag, error) {
-	tags := []Tag{}
+func GetTagsByTaskID(taskID int64) ([]string, error) {
+	tags := []string{}
 
-	err := DBConn.Model(&tags).
-		Where("tag.task_id = ?", taskID).
-		Select()
+	_, err := DBConn.Query(&tags, "SELECT tag.name FROM tag WHERE tag.task_id = ?", taskID)
 	if err != nil {
 		return nil, err
 	}
--- model/task.go
+++ model/task.go
@@ -5,55 +5,147 @@ import (
 	"gopkg.in/pg.v4/orm"
 )
 
-type Task struct {
+// Column flag
+const (
+	CTaskUserID = 1 << iota
+
+	CTaskID
+	CTaskSubject
+	CTaskDue
+	CTaskPriority
+	CTaskReminder
+	CTaskNext
+	CTaskNote
+	CTaskParentTaskID
+
+	CTaskTags
+
+	CTaskSubTaskIDs
+)
+
+// Column name string
+var (
+	ShortColumnNames = map[string]string{
+		"UserID":       "user_id",
+		"ID":           "id",
+		"Subject":      "subject",
+		"Due":          "due",
+		"Priority":     "priority",
+		"Reminder":     "reminder",
+		"Next":         "next",
+		"Note":         "note",
+		"ParentTaskID": "parent_task_id",
+	}
+
+	FullColumnNames = map[string]string{
+		"UserID":       "task.user_id",
+		"ID":           "task.id",
+		"Subject":      "task.subject",
+		"Due":          "task.due",
+		"Priority":     "task.priority",
+		"Reminder":     "task.reminder",
+		"Next":         "task.next",
+		"Note":         "task.note",
+		"ParentTaskID": "task.parent_task_id",
+	}
+)
+
+type taskNormalColumns struct {
 	TableName struct{} `sql:"task"`
 
 	UserID string
 
-	ID       int64 `sql:",pk"`
-	Subject  string
-	Due      Time
-	Priority int
-	Reminder Time
-	Next     Time
-	Note     string
-
-	Tags Tags
-
+	ID           int64 `sql:",pk"`
+	Subject      string
+	Due          Time
+	Priority     int
+	Reminder     Time
+	Next         Time
+	Note         string
 	ParentTaskID int64 `sql:",null"`
-	ParentTask   *Task
-	SubTasks     []Task `pg:",fk:ParentTask"`
+}
+
+type Task struct {
+	TableName struct{} `sql:"task"`
+
+	taskNormalColumns
+
+	Tags []string `pg:",array"`
+
+	SubTaskIDs []int64 `sql:"sub_task_ids" pg:",array"`
+}
+
+func genNormalColumnList(fullName bool, columns int) []string {
+	var columnNames map[string]string
+	if fullName {
+		columnNames = FullColumnNames
+	} else {
+		columnNames = ShortColumnNames
+	}
+
+	c := make([]string, 0, len(columnNames)) // TODO: pop count
+
+	if columns&CTaskUserID != 0 {
+		c = append(c, columnNames["UserID"])
+	}
+	if columns&CTaskID != 0 {
+		c = append(c, columnNames["ID"])
+	}
+	if columns&CTaskSubject != 0 {
+		c = append(c, columnNames["Subject"])
+	}
+	if columns&CTaskDue != 0 {
+		c = append(c, columnNames["Due"])
+	}
+	if columns&CTaskPriority != 0 {
+		c = append(c, columnNames["Priority"])
+	}
+	if columns&CTaskReminder != 0 {
+		c = append(c, columnNames["Reminder"])
+	}
+	if columns&CTaskNext != 0 {
+		c = append(c, columnNames["Next"])
+	}
+	if columns&CTaskNote != 0 {
+		c = append(c, columnNames["Note"])
+	}
+	if columns&CTaskParentTaskID != 0 {
+		c = append(c, columnNames["ParentTaskID"])
+	}
+
+	return c
+}
+
+func joinTags(q *orm.Query) *orm.Query {
+	return q.Column("task_tag.tags").
+		Join("LEFT JOIN task_tag ON task.id = task_tag.task_id")
 }
 
 func updateTags(tx *pg.Tx, task Task) error {
 	if len(task.Tags) == 0 {
-		_, err := tx.Exec("DELETE FROM tag WHERE tag.task_id = ?;", task.ID)
+		_, err := tx.Exec("DELETE FROM tag WHERE tag.task_id = ?", task.ID)
 		if err != nil {
 			return err
 		}
 		return nil
 	}
 
-	tagNames := make([]string, len(task.Tags))
-	for n := 0; n < len(task.Tags); n++ {
-		task.Tags[n].UserID = task.UserID
-
-		task.Tags[n].TaskID = task.ID
-
-		tagNames[n] = task.Tags[n].Name
-	}
-
-	_, err := tx.Exec("DELETE FROM tag WHERE tag.task_id = ? AND tag.name NOT IN (?);", task.ID, pg.In(tagNames))
+	_, err := tx.Exec("DELETE FROM tag WHERE tag.task_id = ? AND tag.name NOT IN (?)", task.ID, pg.In(task.Tags))
 	if err != nil {
 		return err
 	}
 
-	_, err = tx.Model(&task.Tags).OnConflict("DO NOTHING").Create()
+	_, err = tx.Exec("INSERT INTO tag (SELECT ?, ?, unnest(?::text[])) ON CONFLICT DO NOTHING", task.UserID, task.ID, pg.Array(task.Tags))
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func joinSubTaskIDs(q *orm.Query) *orm.Query {
+	return q.Column("task_sub_task_id.sub_task_ids").
+		Join("LEFT JOIN task_sub_task_id ON task.id = task_sub_task_id.task_id")
 }
 
 func CreateTask(task Task) error {
@@ -62,7 +154,7 @@ func CreateTask(task Task) error {
 		return err
 	}
 
-	err = tx.Create(&task)
+	err = tx.Create(&task.taskNormalColumns)
 	if err != nil {
 		tx.Rollback()
 		return err
@@ -80,16 +172,20 @@ func CreateTask(task Task) error {
 	return err
 }
 
-func GetTask(userID string, taskID int64, columns ...string) (Task, error) {
-	task := Task{
-		UserID: userID,
-		ID:     taskID,
+func GetTask(userID string, taskID int64, columns int) (Task, error) {
+	task := Task{}
+
+	q := DBConn.Model(&task).Column("task.*")
+
+	if columns&CTaskTags != 0 {
+		q = joinTags(q)
+	}
+	if columns&CTaskSubTaskIDs != 0 {
+		q = joinSubTaskIDs(q)
 	}
 
-	err := DBConn.Model(&task).
-		Column(columns...).
-		Where("task.id = ?", taskID).
-		Where("task.user_id = ?", userID).
+	err := q.
+		Where("task.id = ? AND task.user_id = ?", taskID, userID).
 		Select()
 	if err != nil {
 		return Task{}, err
@@ -98,11 +194,41 @@ func GetTask(userID string, taskID int64, columns ...string) (Task, error) {
 	return task, nil
 }
 
-func GetTasksByUserID(userID string, columns ...string) ([]Task, error) {
+func GetTasksByID(userID string, ids []int64, columns int) ([]Task, error) {
 	tasks := []Task{}
 
-	err := DBConn.Model(&tasks).
-		Column(columns...).
+	q := DBConn.Model(&tasks).Column("task.*")
+
+	if columns&CTaskTags != 0 {
+		q = joinTags(q)
+	}
+	if columns&CTaskSubTaskIDs != 0 {
+		q = joinSubTaskIDs(q)
+	}
+
+	err := q.
+		Where("task.user_id = ? AND task.id IN (?)", userID, pg.In(ids)).
+		Select()
+	if err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
+func GetTasksByUserID(userID string, columns int) ([]Task, error) {
+	tasks := []Task{}
+
+	q := DBConn.Model(&tasks).Column("task.*")
+
+	if columns&CTaskTags != 0 {
+		q = joinTags(q)
+	}
+	if columns&CTaskSubTaskIDs != 0 {
+		q = joinSubTaskIDs(q)
+	}
+
+	err := q.
 		Where("task.user_id = ?", userID).
 		Select()
 	if err != nil {
@@ -112,14 +238,21 @@ func GetTasksByUserID(userID string, columns ...string) ([]Task, error) {
 	return tasks, nil
 }
 
-func GetTasksByTag(userID string, tagName string, columns ...string) ([]Task, error) {
+func GetTasksByTag(userID string, tagName string, columns int) ([]Task, error) {
 	tasks := []Task{}
 
-	err := DBConn.Model(&tasks).
-		Column(columns...).
-		Join("JOIN tag ON task.id = tag.task_id").
-		Where("task.user_id = ?", userID).
-		Where("tag.name = ?", tagName).
+	q := DBConn.Model(&tasks).Column("task.*")
+
+	if columns&CTaskTags != 0 {
+		q = joinTags(q)
+	}
+	if columns&CTaskSubTaskIDs != 0 {
+		q = joinSubTaskIDs(q)
+	}
+
+	err := q.
+		Join("INNER JOIN tag ON task.id = tag.task_id").
+		Where("task.user_id = ? AND tag.name = ?", userID, tagName).
 		Select()
 	if err != nil {
 		return nil, err
@@ -128,11 +261,19 @@ func GetTasksByTag(userID string, tagName string, columns ...string) ([]Task, er
 	return tasks, nil
 }
 
-func GetTasksByFunc(userID string, f func(q *orm.Query) *orm.Query, columns ...string) ([]Task, error) {
+func GetTasksByFunc(userID string, f func(q *orm.Query) *orm.Query, columns int) ([]Task, error) {
 	tasks := []Task{}
 
-	err := DBConn.Model(&tasks).
-		Column(columns...).
+	q := DBConn.Model(&tasks).Column("task.*")
+
+	if columns&CTaskTags != 0 {
+		q = joinTags(q)
+	}
+	if columns&CTaskSubTaskIDs != 0 {
+		q = joinSubTaskIDs(q)
+	}
+
+	err := q.
 		Where("task.user_id = ?", userID).
 		Apply(f).
 		Select()
@@ -143,30 +284,26 @@ func GetTasksByFunc(userID string, f func(q *orm.Query) *orm.Query, columns ...s
 	return tasks, nil
 }
 
-func UpdateTask(task Task, columns ...string) error {
+func UpdateTask(task Task, columns int) error {
 	tx, err := DBConn.Begin()
 	if err != nil {
 		return err
 	}
 
 	_, err = tx.Model(&task).
-		Column(columns...).
-		Where("task.id = ?", task.ID).
-		Where("task.user_id = ?", task.UserID).
+		Column(genNormalColumnList(false, columns)...).
+		Where("task.id = ? AND task.user_id = ?", task.ID, task.UserID).
 		Update()
 	if err != nil {
 		tx.Rollback()
 		return err
 	}
 
-	for _, column := range columns {
-		if column == "Tags" {
-			err = updateTags(tx, task)
-			if err != nil {
-				tx.Rollback()
-				return err
-			}
-			break
+	if columns&CTaskTags != 0 {
+		err = updateTags(tx, task)
+		if err != nil {
+			tx.Rollback()
+			return err
 		}
 	}
 
@@ -176,16 +313,14 @@ func UpdateTask(task Task, columns ...string) error {
 
 func DeleteTask(task Task) error {
 	_, err := DBConn.Model(&task).
-		Where("task.id = ?", task.ID).
-		Where("task.user_id = ?", task.UserID).
+		Where("task.id = ? AND task.user_id = ?", task.ID, task.UserID).
 		Delete()
 	return err
 }
 
 func DoneTask(task Task) error {
 	err := DBConn.Model(&task).
-		Where("task.id = ?", task.ID).
-		Where("task.user_id = ?", task.UserID).
+		Where("task.id = ? AND task.user_id = ?", task.ID, task.UserID).
 		Select()
 	if err != nil {
 		return err
@@ -207,5 +342,5 @@ func DoneTask(task Task) error {
 	}
 	task.Next.Set(task.Next.Get() + delta)
 
-	return UpdateTask(task, "due", "reminder", "next")
+	return UpdateTask(task, CTaskDue|CTaskReminder|CTaskNext)
 }
